bb8f71dc849829647c652dafd28f7ee5affe3b49
harishtamminana <harishtamminana@gmail.com>
2025-12-30 01:01:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -60,7 +60,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://harishtamminana_db_user:<db_password>@cluster0.hy8rfow.mongodb.net/?appName=Cluster0")   # IF Multiple Database Is False Then Fill Only This Database Url.
-DATABASE_NAME = environ.get('DATABASE_NAME', "HT FILEZ GROUP")
+DATABASE_NAME = environ.get('DATABASE_NAME', "HTFILEZGROUP")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'HT FILEZ GROUP')
 
 MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
@@ -196,6 +196,7 @@ else:
 # Don't Remove Credit @VJ_Bots
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
